746a3ff1f24225d8d57a41a0076c457301eeee50
Use your GitHub username <Enter your email address>
2020-10-15 20:57:30 -0400

    Lesson 5

--- lesson 5/js/js.js
+++ lesson 5/js/js.js
@@ -11,3 +11,13 @@ const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
 
 document.getElementById("copyright-year").innerText = new Date().getFullYear();
+
+function pancake() {
+    var daynumber = new Date();
+
+if (daynumber.getDay() == 5){
+        document.getElementsByClassName("pancakes");
+
+    document.querySelector(".pancakes").style.display = "block";
+}
+}
