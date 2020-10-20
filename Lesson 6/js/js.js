9a00e82212b90d877b0aff39d9afb5f430478e4e
Use your GitHub username <Enter your email address>
2020-10-20 16:55:51 -0400

    lesson 6 start

---
+++ Lesson 6/js/js.js
@@ -0,0 +1,24 @@
+var lastEdit = document.lastModified;
+document.getElementById("lastModified").textContent = lastEdit;
+
+function toggleMenu() {
+    document
+    .getElementsByClassName("navigation")[0]
+    .classList.toggle("responsive");
+}
+
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+document.getElementById("copyright-year").innerText = new Date().getFullYear();
+
+function pancake() {
+    var daynumber = new Date();
+
+if (daynumber.getDay() == 5){
+        document.getElementsByClassName("pancakes");
+
+    document.querySelector(".pancakes").style.display = "block";
+}
+}
+
