bed2d1b4a7c7b13928f43a7aa61fdaa3a7a19c0b
Use your GitHub username <Enter your email address>
2020-11-11 14:45:26 -0500

    prophets

---
+++ lesson-9/js/windchill.js
@@ -0,0 +1,18 @@
+function getwindchill() {
+let t = parseFloat(document.getElementById("t").textContent);
+let s = parseFloat(document.getElementById("s").textContent);
+var f = Windchill(t, s);
+
+}
+
+function Windchill(t, s) {
+var b = 35.74 + (0.6215 * t) - 35.75 * s ** 0.16 + 0.4275 * t * s ** 0.16;
+var f = Math.round(10 * b) / 10;
+var na = "N/A"
+if (t <= 50 && s >= 3) {
+    document.getElementById("WC").innerHTML = "Wind Chill: " + f
+}
+else {
+    document.getElementById("WC").innerHTML = "Wind Chill: " + na;
+}
+}
